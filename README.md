f57467ccbf9084236cfde4167f2677d14b1fb6b2
Dan Buda <danbuda@gmail.com>
2017-06-11 17:39:18 -0500

    data showing up and started styling

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Messing around with the Star Wars API
+# Messing around with the Star Wars API & Async/Await
+
+Fun API to mess around with while I work on a little Async/Await stuff.
--- src/index.js
+++ src/index.js
@@ -1,142 +1,88 @@
 import './styles/main.css';
 import axios from 'axios';
-
-// get planets from each page and push into main array
-// THEN sort the full array
-// THEN append to #planet-select
-
+import _ from 'lodash';
 
 const planetSelect = document.getElementById('planet-select');
+const dataDiv = document.getElementById('data');
+const ROOT_URL = 'http://swapi.co/api';
+let allPlanets = [];
+
 planetSelect.addEventListener('change', () => {
-	console.log(planetSelect.value);
+	// console.log(planetSelect.value);
+	console.log(allPlanets); // showing up as empty array - fixed now?
+
+	const key = _.findKey(allPlanets, { name: planetSelect.value });
+
+	// console.log('key: ', key);
+
+	const planetName = document.querySelector('.planet-name');
+	const diameter = document.querySelector('.planet-diameter');
+	const gravity = document.querySelector('.planet-gravity');
+	const orbital = document.querySelector('.planet-orbital-period');
+	const rotation = document.querySelector('.planet-rotation-period');
+	const climate = document.querySelector('.planet-climate');
+	const terrain = document.querySelector('.planet-terrain');
+	const water = document.querySelector('.planet-surface-water');
+	const population = document.querySelector('.planet-population');
+	
+	planetName.innerHTML = allPlanets[key].name;
+	diameter.innerHTML = `Diameter: ${allPlanets[key].diameter} kilometers`;
+	gravity.innerHTML = `Gravity: ${allPlanets[key].gravity} G(s)`;
+	orbital.innerHTML = `Orbital Period: ${allPlanets[key].orbital_period} days`;
+	rotation.innerHTML = `Rotational Period: ${allPlanets[key].rotation_period} hours`;
+	climate.innerHTML = `Climate: ${allPlanets[key].climate}`;
+	terrain.innerHTML = `Terrain: ${allPlanets[key].terrain}`;
+	water.innerHTML = `Water: ${allPlanets[key].surface_water}%`;
+	population.innerHTML = `Population: ${allPlanets[key].population}`;
+
+	dataDiv.style.display = 'block';
 });
 
-const logPlanet = () => {
-	console.log(planetSelect.value);
-};
-
-// Try using node 8 async-await or promises to see
-// if that makes it work?
-
-// Need to figure out why console.log(planets) works
-// but I can't actually use the planets array
-
-// Also remember that the planets.sort and planets.forEach
-// aren't even working, or console logging at all
-
-
-// Try from this stack overflow question:
-// https://stackoverflow.com/questions/14220321/how-do-i-return-the-response-from-an-asynchronous-call
-
-const ROOT_URL = 'http://swapi.co/api';
-const allPlanets = [];
-
 const getPlanets = async (pages) => {
+	
+
 	try {
-		let allPlanets = [];
+		let planets = [];
 		
 		for (let i = 1; i < pages + 1; i++) {
 			const response = await axios.get(`${ROOT_URL}/planets/?page=${i}`);
 
 			for (let j = 0; j < response.data.results.length; j++) {
-				allPlanets.push(response.data.results[j].name);
+				planets.push(response.data.results[j]);
 			}
 		}
-		return allPlanets;
+
+		return planets;
 	} catch (e) {
-			throw new Error('Get failed');
+			throw new Error('Request unsuccessul.');
 	}
 };
 
-
 const sortPlanets = async (pages) => {
 	const planets = await getPlanets(pages);
-	const sortedPlanets = planets.sort();
+	const sortedPlanets = await planets.sort((a, b) => {
+		if (a.name.toLowerCase() < b.name.toLowerCase()) {
+			return -1;
+		} else if (a.name.toLowerCase() > b.name.toLowerCase()) {
+			return 1;
+		}
+		return 0;
+	});
+
 	return sortedPlanets;
 };
 
-
 sortPlanets(7).then((status) => {
-	status.forEach((planet) => {
+	// console.log('status: ', status);
+	// allPlanets = status;
+	// console.log(allPlanets);
+	status.forEach((planet, i) => {
 		const option = document.createElement('option');
-		option.textContent = planet;
-		option.value = planet;
+		option.textContent = planet.name;
+		option.value = planet.name;
 		planetSelect.appendChild(option);
 	});
-
-
-
-	// allPlanets.forEach((planet) => {
-	// 	const option = document.createElement('option');
-	// 	option.textContent = planet;
-	// 	planetSelect.appendChild(option);
-	// });
-	
+	return allPlanets = status;
 }).catch((e) => {
 	console.log(e.message);
 });
-
-// const getPlanets = async (page) => {
-// 	try {
-// 		const response = await axios.get(`${ROOT_URL}/planets/?page=${page}`);
-// 		return planets.push(response.data.results);
-// 	} catch (e) {
-// 		throw new Error();
-// 	}
-// };
-
-// for (let i = 1; i < 8; i++) {
-// 	getPlanets(i);
-// }
-
-// planets.sort();
-
-// console.log(planets);
-// const planetSelect = document.getElementById('planet-select');
-
-
-// const planets = [];
-
-// function getSrc(page) {
-// 	return axios.get(`${ROOT_URL}/planets/?page=${page}`);
-// 	console.log('1');
-// };
-
-// for (let i = 1; i < 8; i++) {
-// 	getSrc(i).then((res) => {
-// 		return res.data.results.map((planet) => {
-// 			planets.push(planet);
-// 		});
-// 	});
-// };
-
-
-
-	// axios.get(`${ROOT_URL}/planets/?page=${i}`)
-	// .then((res) => {
-	// 	res.data.results.map((planet) => {
-	// 		planets.push(planet);
-	// 	});
-	// }).catch((e) => {
-	// 	console.log(e)
-	// });
-
-
-// sort planets
-// planets.sort((a, b) => {
-// 	console.log(a);
-// 	if (a.name < b.name) {
-// 		return -1;
-// 	} else if (a.name > b.name) {
-// 		return 1;
-// 	}
-// 	return 0;
-// });
-// append each planet name to the select after adding <option>
-
-// console.log(planets);
-
-// console.log(planets[1].name);
-// planets.forEach((planet) => {
-// 	console.log(planet);
-// });
